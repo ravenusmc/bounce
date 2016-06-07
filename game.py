c1bea0a381c08005888d4133e5ef5ca330097160
ravenusmc <mcuddy77@gmail.com>
2016-06-07 11:47:37 -0400

    ball now moves all around the screen at a fast pace

--- game.py
+++ game.py
@@ -22,9 +22,12 @@ class Ball:
     self.canvas = canvas
     self.id = canvas.create_oval(10,10,25,25, fill=color)
     self.canvas.move(self.id, 245, 100)
-    self.x = 0
-    self.y = -1
+    starts = [-3, -2, -1, 1, 2, 3]
+    random.shuffle(starts)
+    self.x = starts[0]
+    self.y = -3
     self.canvas_height = self.canvas.winfo_height()
+    self.canvas_width = self.canvas.winfo_width()
 
   #making te ball move
   def draw(self):
@@ -32,9 +35,13 @@ class Ball:
     self.canvas.move(self.id, self.x, self.y)
     pos = self.canvas.coords(self.id)
     if pos[1] <= 0:
-      self.y = 1
+      self.y = 3
     if pos[3] >= self.canvas_height:
-      self.y = -1
+      self.y = -3
+    if pos[0] <= 0:
+      self.x = 3
+    if pos[2] >= self.canvas_width:
+      self.x = -3
 
 ball = Ball(canvas, 'blue')
 
