b0ef56749d3e37732564f4d1502b537b701b20c7
ravenusmc <mcuddy77@gmail.com>
2016-06-10 11:31:30 -0400

    refractured code

--- game.py
+++ game.py
@@ -2,6 +2,9 @@ from tkinter import *
 import random 
 import time 
 
+from ball import Ball
+from paddle import Paddle 
+
 tk = Tk()
 #The title is placed at the top of the sqare with the below line. 
 tk.title("Bounce Game")
@@ -16,75 +19,7 @@ canvas.pack()
 #This line tells tkinter to init itself for the animation in the game. 
 tk.update()
 
-############# Classes start ############
-
-#Creating the ball class. 
-class Ball:
-  def __init__(self, canvas, paddle, color):
-    self.canvas = canvas
-    self.paddle = paddle 
-    self.id = canvas.create_oval(10,10,25,25, fill=color)
-    self.canvas.move(self.id, 245, 100)
-    starts = [-3, -2, -1, 1, 2, 3]
-    random.shuffle(starts)
-    self.x = starts[0]
-    self.y = -3
-    self.canvas_height = self.canvas.winfo_height()
-    self.canvas_width = self.canvas.winfo_width()
-    self.hit_bottom = False 
-
-  def hit_paddle(self, pos):
-    paddle_pos = self.canvas.coords(self.paddle.id)
-    if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
-      if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
-        return True
-    return False
-
-  #making te ball move
-  def draw(self):
-    #The zero says do not move horizontal and the -1 says move UP the screen 
-    self.canvas.move(self.id, self.x, self.y)
-    pos = self.canvas.coords(self.id)
-    if pos[1] <= 0:
-      self.y = 3
-    if pos[3] >= self.canvas_height:
-      self.hit_bottom = True
-    if self.hit_paddle(pos) == True:
-      self.y = -3 
-    if pos[0] <= 0:
-      self.x = 3
-    if pos[2] >= self.canvas_width:
-      self.x = -3
-
-#Paddle class
-class Paddle:
-  def __init__(self, canvas, color):
-    self.canvas = canvas
-    self.id = canvas.create_rectangle(0,0,100,10, fill=color)
-    self.canvas.move(self.id, 200, 300)
-    self.x = 0
-    self.canvas_width = self.canvas.winfo_width()
-    self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
-    self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
-
-  def draw(self):
-    self.canvas.move(self.id, self.x, 0)
-    pos = self.canvas.coords(self.id)
-    if pos[0] <= 0:
-      self.x = 0
-    elif pos[2] >= self.canvas_width:
-      self.x = 0
-
-  def turn_left(self, evt):
-    self.x = -2
-
-  def turn_right(self, evt):
-    self.x = 2
-
-
-
-####### Classes End #######################
-
+#These two lines create a ball and paddle object based on the ball and paddle classes.
 paddle = Paddle(canvas, "red")
 ball = Ball(canvas, paddle, 'blue')
 
